e5449bd7b4312213197d1a3d6276bae3bf879483
David Vasconcelos <dsv10@hotmail.com>
2019-04-14 18:24:57 -0300

    swagger

--- src/main/java/br/com/fiap/orderservice/controller/OrderController.java
+++ src/main/java/br/com/fiap/orderservice/controller/OrderController.java
@@ -2,6 +2,7 @@ package br.com.fiap.orderservice.controller;
 
 import br.com.fiap.orderservice.model.Order;
 import br.com.fiap.orderservice.repository.OrderRepository;
+import io.swagger.annotations.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,19 +18,27 @@ import java.net.URI;
 @Slf4j
 @RestController
 @RequestMapping("/orders")
+@Api(value = "Order", description = "Order Service REST API")
 public class OrderController {
 
     @Autowired
     private OrderRepository repository;
 
+    @ApiOperation(httpMethod = "GET", value = "Método get para buscar pedido filtrando por id")
+            @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Retorna um Order com uma mensagem de sucesso", response = Order.class)})
     @GetMapping("/{id}")
-    public ResponseEntity<Order> findById(@PathVariable("id") Long id) {
+    public ResponseEntity<Order> findById(@ApiParam( value = "Order Id", required = true)
+                                          @PathVariable("id") Long id) {
 
         final Order order = repository.getById(id);
 
         return new ResponseEntity<>(order, HttpStatus.OK);
     }
 
+    @ApiOperation(httpMethod = "Post", value = "Método post inserir um pedido")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Retorna o path para pesquisa")})
     @PostMapping
     public ResponseEntity<Order> save(@Valid @RequestBody Order order) {
 
@@ -40,6 +49,9 @@ public class OrderController {
         return ResponseEntity.created(location).build();
     }
 
+    @ApiOperation(httpMethod = "Put", value = "Método put para alterar um pedido por id")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Retorna uma mensagem de sucesso")})
     @PutMapping("/{id}")
     public ResponseEntity<Order> update(@PathVariable("id") Long id, @Valid @RequestBody Order order) {
 
@@ -48,6 +60,9 @@ public class OrderController {
         return new ResponseEntity(HttpStatus.OK);
     }
 
+    @ApiOperation(httpMethod = "Delete", value = "Método delete para apagar um pedido por id")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Retorna uma mensagem de sucesso")})
     @DeleteMapping("/{id}")
     public ResponseEntity<Order> delete(@PathVariable("id") Long id) {
 
--- src/main/java/br/com/fiap/orderservice/model/Order.java
+++ src/main/java/br/com/fiap/orderservice/model/Order.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
 import java.math.BigDecimal;
@@ -20,18 +21,24 @@ import java.util.List;
 @JsonInclude(Include.NON_EMPTY)
 public class Order {
 
+    @ApiModelProperty(notes = "The database generated Order ID")
     private Long id;
 
+    @ApiModelProperty( notes = "User name", required = true)
     private String fullName;
 
+    @ApiModelProperty( notes = "User E-mail", required = true)
     private String email;
 
+    @ApiModelProperty( notes = "The order delivery address", required = true)
     private String shippingAddress;
 
+    @ApiModelProperty( notes = "The order itens", required = true)
     private List<Item> items = new ArrayList<>();
 
     private BigDecimal totalPrice;
 
+    @ApiModelProperty( notes = "The order payment mode", required = true)
     private PaymentMode paymentMode;
 
     @JsonFormat(pattern = "MM/dd/yyyy HH:mm:ss.SSS",
@@ -40,8 +47,10 @@ public class Order {
             timezone = "Brazil/East")
     private Date orderDate;
 
+    @ApiModelProperty( notes = "The order status", required = true)
     private Status status;
 
+    @ApiModelProperty( notes = "The order transaction", required = true)
     private Transaction transaction;
 
 
